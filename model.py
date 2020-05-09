4c15b1a4de276aa7f34948fe3ac89daf93f210c3
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-05-09 12:23:12 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import numpy as np
 
 import tensorflow as tf
 
-#config = tf.compat.v1.ConfigProto()
-#config.gpu_options.per_process_gpu_memory_fraction = 0.15
-#session = tf.compat.v1.Session(config=config)
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.15
+session = tf.compat.v1.Session(config=config)
 
 
 class FacialExpressionModel(object):
